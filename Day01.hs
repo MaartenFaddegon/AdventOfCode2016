efd64e7213325ccef387a6286b49a12339b23258
Maarten Faddegon <mf357@kent.ac.uk>
2016-12-01 21:37:08 +0100

    Day 1 solution 2

--- Day01.hs
+++ Day01.hs
@@ -1,12 +1,12 @@
-data Rotation = R | L deriving Show
+data Rotation = R | L deriving (Show, Eq)
 
-data Instruction = I Rotation Int deriving Show
+data Instruction = I Rotation Int deriving (Show, Eq)
 
-data Direction = N | E | S | W deriving Show
+data Direction = N | E | S | W deriving (Show, Eq)
 
 type Position = (Int, Int)
 
-data State = State{direction :: Direction, position :: Position} deriving Show
+data State = State{direction :: Direction, position :: Position} deriving (Show, Eq)
 
 instructions = [I R 3, I L 2, I L 2, I R 4, I L 1, I R 2, I R 3, I R 4, I L 2, I R 4, I L 2, I L 5, I L 1, I R 5, I R 2, I R 2, I L 1, I R 4, I R 1, I L 5, I L 3, I R 4, I R 3, I R 1, I L 1, I L 5, I L 4, I L 2, I R 5, I L 3, I L 4, I R 3, I R 1, I L 3, I R 1, I L 3, I R 3, I L 4, I R 2, I R 5, I L 190, I R 2, I L 3, I R 47, I R 4, I L 3, I R 78, I L 1, I R 3, I R 190, I R 4, I L 3, I R 4, I R 2, I R 5, I R 3, I R 4, I R 3, I L 1, I L 4, I R 3, I L 4, I R 1, I L 4, I L 5, I R 3, I L 3, I L 4, I R 1, I R 2, I L 4, I L 3, I R 3, I R 3, I L 2, I L 5, I R 1, I L 4, I L 1, I R 5, I L 5, I R 1, I R 5, I L 4, I R 2, I L 2, I R 1, I L 5, I L 4, I R 4, I R 4, I R 3, I R 2, I R 3, I L 1, I R 4, I R 5, I L 2, I L 5, I L 4, I L 1, I R 4, I L 4, I R 4, I L 4, I R 1, I R 5, I L 1, I R 1, I L 5, I R 5, I R 1, I R 1, I L 3, I L 1, I R 4, I L 1, I L 4, I L 4, I L 3, I R 1, I R 4, I R 1, I R 1, I R 2, I L 5, I L 2, I R 4, I L 1, I R 3, I L 5, I L 2, I R 5, I L 4, I R 5, I L 5, I R 3, I R 4, I L 3, I L 3, I L 2, I R 2, I L 5, I L 5, I R 3, I R 4, I R 3, I R 4, I R 3, I R 1]
 
@@ -49,4 +49,37 @@ ex3' = distance s_0 ex3 == 12
 
 solution1 = distance s_0 s_z
 
-main = print solution1
+-----
+
+move' :: Instruction -> State -> [State]
+move' (I _ d) (State N (x,y)) = [State N (x,y+d') | d' <- [1..d]]
+move' (I _ d) (State E (x,y)) = [State E (x+d',y) | d' <- [1..d]]
+move' (I _ d) (State S (x,y)) = [State S (x,y-d') | d' <- [1..d]]
+move' (I _ d) (State W (x,y)) = [State W (x-d',y) | d' <- [1..d]]
+
+ap' :: Instruction -> [State] -> [State]
+ap' i = (move' i) . (rotate i) . last
+
+eval' :: [Instruction] -> [State]
+eval' = concat . scanl (flip ap') [s_0]
+
+twice []                   = Nothing
+twice (x:xs) | x `elem` xs = Just x
+             | otherwise   = twice xs
+
+
+distance' :: Position -> Position -> Int
+distance' (a,b) (c,d) = abs(a-c) + abs(b-d)
+
+ex4 = map position $ eval' [I R 8, I R 4, I R 4, I R 8]
+ex4' = twice ex4
+
+solution2 = do 
+  p <- twice . map position . eval' $ instructions
+  return $ distance' (0,0) p
+
+main = do
+  putStr "Solution 1: "
+  print solution1
+  putStr "Solution 2: "
+  print solution2
