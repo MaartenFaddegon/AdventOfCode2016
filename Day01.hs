badaaa7b95c0c0dff595e3fa32abe77338276cbc
Maarten Faddegon <mf357@kent.ac.uk>
2016-12-01 21:12:19 +0100

    Day 1 solution 1

---
+++ Day01.hs
@@ -0,0 +1,52 @@
+data Rotation = R | L deriving Show
+
+data Instruction = I Rotation Int deriving Show
+
+data Direction = N | E | S | W deriving Show
+
+type Position = (Int, Int)
+
+data State = State{direction :: Direction, position :: Position} deriving Show
+
+instructions = [I R 3, I L 2, I L 2, I R 4, I L 1, I R 2, I R 3, I R 4, I L 2, I R 4, I L 2, I L 5, I L 1, I R 5, I R 2, I R 2, I L 1, I R 4, I R 1, I L 5, I L 3, I R 4, I R 3, I R 1, I L 1, I L 5, I L 4, I L 2, I R 5, I L 3, I L 4, I R 3, I R 1, I L 3, I R 1, I L 3, I R 3, I L 4, I R 2, I R 5, I L 190, I R 2, I L 3, I R 47, I R 4, I L 3, I R 78, I L 1, I R 3, I R 190, I R 4, I L 3, I R 4, I R 2, I R 5, I R 3, I R 4, I R 3, I L 1, I L 4, I R 3, I L 4, I R 1, I L 4, I L 5, I R 3, I L 3, I L 4, I R 1, I R 2, I L 4, I L 3, I R 3, I R 3, I L 2, I L 5, I R 1, I L 4, I L 1, I R 5, I L 5, I R 1, I R 5, I L 4, I R 2, I L 2, I R 1, I L 5, I L 4, I R 4, I R 4, I R 3, I R 2, I R 3, I L 1, I R 4, I R 5, I L 2, I L 5, I L 4, I L 1, I R 4, I L 4, I R 4, I L 4, I R 1, I R 5, I L 1, I R 1, I L 5, I R 5, I R 1, I R 1, I L 3, I L 1, I R 4, I L 1, I L 4, I L 4, I L 3, I R 1, I R 4, I R 1, I R 1, I R 2, I L 5, I L 2, I R 4, I L 1, I R 3, I L 5, I L 2, I R 5, I L 4, I R 5, I L 5, I R 3, I R 4, I L 3, I L 3, I L 2, I R 2, I L 5, I L 5, I R 3, I R 4, I R 3, I R 4, I R 3, I R 1]
+
+rotate :: Instruction -> State -> State
+rotate (I R _) (State N p) = State E p
+rotate (I R _) (State E p) = State S p
+rotate (I R _) (State S p) = State W p
+rotate (I R _) (State W p) = State N p
+rotate (I L _) (State N p) = State W p
+rotate (I L _) (State E p) = State N p
+rotate (I L _) (State S p) = State E p
+rotate (I L _) (State W p) = State S p
+
+move :: Instruction -> State -> State
+move (I _ d) (State N (x,y)) = State N (x,y+d)
+move (I _ d) (State E (x,y)) = State E (x+d,y)
+move (I _ d) (State S (x,y)) = State S (x,y-d)
+move (I _ d) (State W (x,y)) = State W (x-d,y)
+
+ap :: Instruction -> State -> State
+ap i = (move i) . (rotate i)
+
+eval :: [Instruction] -> State
+eval = foldl (flip ap) s_0
+
+s_0 = State N (0,0)
+
+s_z :: State
+s_z = eval instructions
+
+distance :: State -> State -> Int
+distance State{position=(a,b)} State{position=(c,d)} = abs(a-c) + abs(b-d)
+
+ex1  = eval [I R 2, I L 3]
+ex1' = distance s_0 ex1 == 5
+ex2  = eval [I R 2, I R 2, I R 2]
+ex2' = distance s_0 ex2 == 2
+ex3  = eval [I R 5, I L 5, I R 5, I R 3]
+ex3' = distance s_0 ex3 == 12
+
+solution1 = distance s_0 s_z
+
+main = print solution1
