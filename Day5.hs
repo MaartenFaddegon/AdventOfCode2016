a40b2a37dece685aa2270e5837870779325b66f8
Maarten Faddegon <mf357@kent.ac.uk>
2016-12-05 13:53:20 +0100

    Day 5 solution 2

--- Day5.hs
+++ Day5.hs
@@ -1,4 +1,6 @@
 import Data.Hash.MD5
+import Data.Char
+import Data.List
 
 hash :: String -> Int -> String
 hash s i = md5s (Str $ s ++ show i)
@@ -11,6 +13,27 @@ ex1 = take 4 $ search exInput 0
 
 solution1 = take 8 $ search input 0
 
+search2 :: String -> Int -> [Int] -> [(Int, Char)]
+search2 s i ps = case hash s i of
+  '0':'0':'0':'0':'0':x:y:_ -> case midx x of
+                                 Just i  -> if i `elem` ps then srch ps else (i, y) : srch (i:ps)
+                                 Nothing -> srch ps
+  _                         -> srch ps
+  where
+  srch = search2 s (i+1)
+
+midx c | c >= '0' && c < '8' = Just $ ord c - ord '0'
+       | otherwise           = Nothing
+
+ex2 = search2 exInput 0 []
+
+solution2 = do
+  let xs = take 8 $ search2 input 0 []
+  print xs
+  putStrLn $ "code: " ++ show (map snd . sortOn fst $ xs)
+
 exInput = "abc"
 
 input = "ojvtpuvg"
+
+main = solution2
