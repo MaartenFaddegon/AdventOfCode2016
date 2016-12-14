7c9ca52e5dfe16b0722b12cc3d3ab1f982b98cfd
Maarten Faddegon <mf357@kent.ac.uk>
2016-12-14 14:02:55 +0100

    Day 14 solution 2

--- Day14.hs
+++ Day14.hs
@@ -3,21 +3,24 @@ import Data.List
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
 
-data HashCache = H String (IntMap (Maybe Char, Maybe Char))
+data HashCache = H (Int -> String) (IntMap (Maybe Char, Maybe Char))
 
 ex0 = threeOfTheSame (hash "abc" 0x18)
-ex1 = snd $ getHash (initCache "abc") 0x18
-ex2 = snd $ getHash (initCache "abc") 0x39
-ex3 = snd $ getHash (initCache "abc") 0x816
-ex3' = snd $ search5 (initCache "abc") 0x39 1 'e'
-ex4 = head $ searchKey (initCache "abc") 0
-ex5 = (searchKey (initCache "abc") 0) !! 63
+ex1 = snd $ getHash    (initCache (hash "abc")) 0x18
+ex2 = snd $ getHash    (initCache (hash "abc")) 0x39
+ex3 = snd $ getHash    (initCache (hash "abc")) 0x816
+ex3' = snd $ search5   (initCache (hash "abc")) 0x39 1 'e'
+ex4 = head $ searchKey (initCache (hash "abc")) 0
+ex5 = (searchKey (initCache $ hash "abc") 0) !! 63
 
-main = putStrLn solution1
+solution1 = (searchKey (initCache $ hash "ahsbgdzn") 0) !! 63
 
-solution1 = show k
-  where
-  k = (searchKey (initCache "ahsbgdzn") 0) !! 63
+ex6 = (stretchedHash "abc" 0)
+
+solution2 = (searchKey (initCache $ stretchedHash "ahsbgdzn") 0) !! 63
+
+main = print solution2
+
 
 searchKey :: HashCache -> Int -> [Int]
 searchKey m i = case x of 
@@ -44,10 +47,15 @@ getHash (H s m) i = case IntMap.lookup i m of
   Just x  -> (H s m, x)
   Nothing -> (H s (IntMap.insert i y m), y)
   where
-  h = hash s i
+  h = s i
   y = (threeOfTheSame h, fiveOfTheSame h)
 
 initCache s = H s IntMap.empty
+
+stretchedHash :: String -> Int -> String
+stretchedHash s i  = stretchedHash' 2016 (hash s i)
+stretchedHash' 0 s = s
+stretchedHash' n s = stretchedHash' (n-1) $ md5s (Str s)
 
 hash :: String -> Int -> String
 hash s i = md5s (Str $ s ++ show i)
